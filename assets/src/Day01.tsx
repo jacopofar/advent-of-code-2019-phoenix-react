e55155f116d54b3d153bfd5f689a6d4eabaaa82f
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-01 20:49:49 +0100

    Change textarea size to something nicer

--- assets/src/Day01.tsx
+++ assets/src/Day01.tsx
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme: Theme) =>
     textField: {
         marginLeft: theme.spacing(1),
         marginRight: theme.spacing(1),
-        width: 100
+        width: 300
     },
   }),
 );
---
+++ lib/advent2019_web/controllers/app_controller.ex
@@ -0,0 +1,6 @@
+defmodule Advent2019Web.AppController do
+    use Advent2019Web, :controller
+    def index(conn, _params) do
+        redirect(conn, to: "/index.html")
+      end
+  end
