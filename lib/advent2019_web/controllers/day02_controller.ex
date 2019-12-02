761434ea0e56fabaa8489ab3dc78c5c1992ecfc2
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-02 11:14:14 +0100

    Add implementation for day 02.1

--- lib/advent2019_web/controllers/day02_controller.ex
+++ lib/advent2019_web/controllers/day02_controller.ex
@@ -1,19 +1,55 @@
 defmodule Advent2019Web.Day02Controller do
-    use Advent2019Web, :controller
+  use Advent2019Web, :controller
 
-    def execute1(op_data_map, position, history) do
+  def execute1(op_data_map, position, history) do
+    op = op_data_map[position]
+    op1_pos = op_data_map[position + 1]
+    op2_pos = op_data_map[position + 2]
+    target_pos = op_data_map[position + 3]
 
-    end
+    case op do
+      1 ->
+        # sum
+        execute1(
+          Map.replace!(
+            op_data_map,
+            target_pos,
+            op_data_map[op1_pos] + op_data_map[op2_pos]
+          ),
+          position + 4,
+          history ++ [%{op: "add", target_pos: target_pos, input_pos: [op1_pos, op2_pos]}]
+        )
 
-    def solve1(conn, params) do
-        IO.inspect params["_json"]
-        result = params["_json"]
-        |> Enum.with_index # equivalent to enumerate in Python
-        |> Map.new
-        |> IO.inspect
+      2 ->
+        execute1(
+          Map.replace!(
+            op_data_map,
+            target_pos,
+            op_data_map[op1_pos] * op_data_map[op2_pos]
+          ),
+          position + 4,
+          history ++ [%{op: "mul", target_pos: target_pos, input_pos: [op1_pos, op2_pos]}]
+        )
 
-        # IO.puts "Day 02.1 result: #{result}"
-
-        json(conn, %{result: result})
+      99 ->
+        {op_data_map, position, history}
     end
   end
+
+  def solve1(conn, params) do
+    IO.inspect(params["_json"])
+
+    result =
+      params["_json"]
+      # equivalent to enumerate in Python
+      |> Enum.with_index()
+      |> Enum.map(fn {v, k} -> {k, v} end)
+      |> Map.new()
+      |> IO.inspect()
+
+    {processed_map, _, history} = execute1(result, 0, [])
+    # IO.puts "Day 02.1 result: #{result}"
+    IO.inspect(history)
+    json(conn, %{result: processed_map[0]})
+  end
+end
