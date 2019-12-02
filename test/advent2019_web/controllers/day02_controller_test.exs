b7346c67dbd39047c4e3ecf1aa39c095661e18bb
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-02 11:24:11 +0100

    Add day 02.1 solution, not yet accepted by the checker

--- test/advent2019_web/controllers/day02_controller_test.exs
+++ test/advent2019_web/controllers/day02_controller_test.exs
@@ -3,18 +3,45 @@ defmodule Advent2019Web.Day02ControllerTest do
 
   test "POST /day02/1", %{conn: conn} do
     conn =
-      conn
+      build_conn()
       |> put_req_header("accept", "application/json")
       |> post("/day02/1", %{_json: [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]})
 
     assert json_response(conn, 200) == %{"result" => 3500}
+
+    conn =
+      build_conn()
+      |> put_req_header("accept", "application/json")
+      |> post("/day02/1", %{_json: [1, 0, 0, 0, 99]})
+
+    assert json_response(conn, 200) == %{"result" => 2}
+
+    conn =
+      build_conn()
+      |> put_req_header("accept", "application/json")
+      |> post("/day02/1", %{_json: [2, 3, 0, 3, 99]})
+
+    assert json_response(conn, 200) == %{"result" => 2}
+
+    conn =
+      build_conn()
+      |> put_req_header("accept", "application/json")
+      |> post("/day02/1", %{_json: [2, 4, 4, 5, 99, 0]})
+
+    assert json_response(conn, 200) == %{"result" => 2}
+
+    conn =
+      build_conn()
+      |> put_req_header("accept", "application/json")
+      |> post("/day02/1", %{_json: [2, 4, 4, 5, 99, 0]})
+
+    assert json_response(conn, 200) == %{"result" => 2}
+
+    conn =
+      build_conn()
+      |> put_req_header("accept", "application/json")
+      |> post("/day02/1", %{_json: [1, 1, 1, 4, 99, 5, 6, 0, 99]})
+
+    assert json_response(conn, 200) == %{"result" => 30}
   end
-
-  # test "POST /day01/2", %{conn: conn} do
-  #     conn = conn
-  #         |> put_req_header("accept", "application/json")
-  #         |> post("/day01/2", %{_json: [1, 23, 100, 2]})
-
-  #     assert json_response(conn, 200) == %{"result" => 44}
-  # end
 end
