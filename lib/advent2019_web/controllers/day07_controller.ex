7253e76c52079ba843a3710f5af3beaac1cc86a5
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-08 14:52:08 +0100

    Implement pipeline computer for day 7

--- lib/advent2019_web/controllers/day07_controller.ex
+++ lib/advent2019_web/controllers/day07_controller.ex
@@ -9,7 +9,29 @@ defmodule Advent2019Web.Day07Controller do
    to build more complex structures involving multiple computers.
   """
   def run_computing_element(program, input) do
+    IO.inspect(input)
     {_, _, output, _} = Day05.execute1(Day05.list_to_map(program), 0, input, [], [])
     output
+  end
+
+  @doc """
+  Given a program and a list of lists of initial inputs, run it with the first
+   list of inputs, then calls the program again using the concatenation of the
+   second initial input and the output of the first execution.
+  This is repeated until all initial inputs are consumed, then the output of
+   the last block is returned.
+  """
+  def run_computing_pipeline(program, initial_inputs) do
+    [this_input | remaining_inputs] = initial_inputs
+
+    if length(remaining_inputs) == 0 do
+      # last step, return the final value
+      run_computing_element(program, this_input)
+    else
+      # prepare the new input for the program
+      [next_input | other_inputs] = remaining_inputs
+      intermediate_input = run_computing_element(program, this_input)
+      run_computing_pipeline(program, [next_input ++ intermediate_input] ++ other_inputs)
+    end
   end
 end
