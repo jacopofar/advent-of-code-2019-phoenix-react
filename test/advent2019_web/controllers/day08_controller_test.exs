7b391323f3ed01c0bd63d2fb75474b2aa43a5dde
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-13 17:53:50 +0100

    Expand day 8 test to check intermediate steps

--- test/advent2019_web/controllers/day08_controller_test.exs
+++ test/advent2019_web/controllers/day08_controller_test.exs
@@ -2,12 +2,22 @@ defmodule Advent2019Web.Day08ControllerTest do
   use Advent2019Web.ConnCase
   import Advent2019Web.Day08Controller
 
-  test "can find layer with fewest 0s" do
-    assert layer_with_fewest_zeros(%{
+  test "can represent a space image as list of lists" do
+    assert space_image_as_lists(%{
              h: 2,
              w: 3,
              rawImage: "123456789012"
-           }) == 0
+           }) == [
+             [[1, 2, 3], [4, 5, 6]],
+             [[7, 8, 9], [0, 1, 2]]
+           ]
+  end
+
+  test "can find layer with fewest 0s" do
+    assert layer_with_fewest_zeros([
+             [[1, 2, 3], [4, 5, 6]],
+             [[7, 8, 9], [0, 1, 2]]
+           ]) == 0
   end
 
   test "POST /day08/1", %{conn: conn} do
