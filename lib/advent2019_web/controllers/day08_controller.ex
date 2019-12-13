13ba467250a5bc367b0dc7bebd7f6ea121c76c28
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-13 17:56:32 +0100

    Begin implementing day 8 helpers

---
+++ lib/advent2019_web/controllers/day08_controller.ex
@@ -0,0 +1,12 @@
+defmodule Advent2019Web.Day08Controller do
+  use Advent2019Web, :controller
+
+  @doc """
+  Given a spacve image tell which layer has fewest 0.
+  The input map has the w and h parameters telling the size and the raw
+  string containing the digits in reading order
+  """
+  def layer_with_fewest_zeros(space_image) do
+   # implement this...
+  end
+end
