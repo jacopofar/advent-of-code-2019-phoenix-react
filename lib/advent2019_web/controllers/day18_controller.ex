9f8b868ff45eeada6320cedcc4396c2f3ff4ccd3
Jacopo Farina <jacopo.farina@flixbus.com>
2019-12-24 11:49:10 +0100

    Add test and logic to parse labyrinth for day 18

--- lib/advent2019_web/controllers/day18_controller.ex
+++ lib/advent2019_web/controllers/day18_controller.ex
@@ -4,13 +4,27 @@ defmodule Advent2019Web.Day18Controller do
   @doc """
   From an ASCII labyrinth produce a sparse representation as a map.
   """
+  @spec labyrinth_string_to_map(String.t()) :: map
   def labyrinth_string_to_map(labyrinth) do
-    "implement me!"
+    String.split(labyrinth, "\n")
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, line_idx} ->
+      line
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.map(fn {char, column_idx} ->
+        if char != "." do
+          {{line_idx, column_idx}, char}
+        end
+      end)
+    end)
+    |> Enum.reject(&(&1 == nil))
+    |> Map.new()
   end
-
 
   def solve1(conn, params) do
     _blabla = params["_json"]
+    labyrinth_string_to_map(23)
 
     json(conn, %{
       result: 42
